e040021f753c6fac9c3aac01a9ec61b83e352352
unknown <yomandawgshit@gmail.com>
2019-03-14 17:23:28 +0900

    finish sns

--- first_local/sns/models.py
+++ first_local/sns/models.py
@@ -10,7 +10,7 @@ class Posting(models.Model):
     icon = models.CharField(max_length=20, default='')
     # image = models.ImageField(blank=True, upload_to='postings/%Y%m%d') # 오리지널 저장용
     # Resized image
-    image = ProcessedImageField(upload_to='postings/resize/%Y%m%d', processors=[ResizeToFit(width=960, upscale=False)], format='JPEG')
+    image = ProcessedImageField(blank=True, upload_to='postings/resize/%Y%m%d', processors=[ResizeToFit(width=960, upscale=False)], format='JPEG')
     # image_thumbnail 경로만 설정해놓고, 누가 호출하면 그 때 등장(cache 로 등장)
     image_thumbnail = ImageSpecField(source='image', processors=[ResizeToFit(width=320, upscale=False)], format='JPEG', options={'quality': 60})
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,6 +18,15 @@ class Posting(models.Model):
 
     def __str__(self):
         return f'{self.id}: {self.content[:20]}'
+
+    def save(self, *args, **kwargs):
+        # 여기에 filtering 등 여러가지 기능 구현 가능
+        super().save(*args, **kwargs)
+        print(f'\n=== Saved Posting with id: {self.id} ===')
+        print(f'    content: {self.content}')
+        if self.image:
+            print(f'    image: {self.image.width}px * {self.image.height}px: {round(self.image.size/1024)}kb')
+        print('=================================\n')
 
 
 class Comment(models.Model):
@@ -28,4 +37,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.posting.content[:10]}: {self.content[:20]}'
+
 
--- first_local/sns/views.py
+++ first_local/sns/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Posting, Comment
 
 # Create your views here.
+
+def posting_list(request):
+    postings = Posting.objects.order_by('-updated_at')
+    context = {'postings': postings}
+    return render(request, 'sns/list.html', context)
+
+def posting_detail(request, posting_id):
+    # posting = Posting.objects.get(id=posting_id)
+    posting = get_object_or_404(Posting, id=posting_id)
+    comments = posting.comment_set.order_by('-created_at') # TODO: Duck-typing
+    context = {'posting': posting, 'comments': comments}
+    return render(request, 'sns/detail.html', context)
+
+def create_posting(request):
+    if request.method == 'POST':
+        posting = Posting.objects.create(content=request.POST.get('content'), icon=request.POST.get('icon'), image=request.FILES.get('image'))
+        return redirect('sns:posting_detail', posting.id)
+    else:
+        return redirect('sns:posting_list')
+
+def create_comment(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    if request.method == 'POST':
+        comment = Comment()
+        comment.content = request.POST.get('comment')
+        comment.posting = posting
+        comment.save()
+    return redirect('sns:posting_detail', posting_id)
